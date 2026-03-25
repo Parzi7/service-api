40444070f047d42500b31cb9c0a0085cc90298da
Parzi7 <98183016+Parzi7@users.noreply.github.com>
2026-03-26 00:44:58 +0200

    Connect frontend to backend API

---
+++ my-web-service-ui/src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/status")
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>React + Node 🚀</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
