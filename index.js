14c6c60a28b3a43ebdd1e0a03d96f4afa5986ca5
Parzi7 <98183016+Parzi7@users.noreply.github.com>
2026-03-25 23:55:51 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = 3000;
 
-app.get('/api/status', (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+app.get("/api/status", (req, res) => {
   res.json({
-    status: 'ok',
-    message: 'My web service API is running'
+    status: "ok",
+    message: "Backend is working",
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server started on http://localhost:${PORT}`);
 });
